be8079fb96228cfa186e835bb9020e7ff4c701ec
Anthony Kim <anthonykim02@berkeley.edu>
2017-10-05 17:29:48 -0700

    source sink and ball

---
+++ Assets/Scripts/Source.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Source : MonoBehaviour {
+
+
+	public GameObject ballObject;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DropBall());
+	}
+
+	IEnumerator DropBall() {
+		while(true)
+		{
+			GameObject createdBall = Instantiate(ballObject);
+			createdBall.transform.position = transform.position;
+			createdBall.GetComponent<Ball>().init(new Vector2(0, -1));
+			yield return new WaitForSeconds(1);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
