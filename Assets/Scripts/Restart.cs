ff3c9b82d3ef8202752624dfc07c3801bc3d7b37
zane-c <zanedougall@gmail.com>
2017-10-18 02:44:40 -0700

    added reset button.

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+	void Start () {
+		Button playBtn = gameObject.GetComponent<Button> ();
+		playBtn.onClick.AddListener(() => {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		});
+	}
+}
