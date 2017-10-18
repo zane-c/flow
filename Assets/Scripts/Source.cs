875165a624824c9b0222c0c2928802c051c8983c
zane-c <zanedougall@gmail.com>
2017-10-18 01:43:37 -0700

    Added action menu to top of screen.

--- Assets/Scripts/Source.cs
+++ Assets/Scripts/Source.cs
@@ -5,30 +5,19 @@ using UnityEngine.UI;
 
 public class Source : MonoBehaviour {
 
-
 	public GameObject ballObject;
+	public Button playBtn;
+	private bool isPlaying = false;
 
-	public Button StartButton;
-
-	public bool start = false;
-
-	// Use this for initialization
 	void Start () {
-		// Button startComp = StartButton.GetComponent<Button>();
-		StartCoroutine (DropBall ());
-		// startComp.onClick.AddListener(StartPauseGame);
-	}
-
-	void StartPauseGame() {
-		if (!start) {
-			StartCoroutine (DropBall ());
-			StartButton.GetComponentInChildren<Text>().text = "STOP";
-			start = true;
-		} else {
-			StopAllCoroutines ();
-			StartButton.GetComponentInChildren<Text>().text = "START";
-			start = false;
-		}
+		playBtn.onClick.AddListener(() => {
+			isPlaying = !isPlaying;
+			if (isPlaying) {
+				StartCoroutine(DropBall());
+			} else {
+				StopCoroutine(DropBall());
+			}
+		});
 	}
 
 	IEnumerator DropBall() {
@@ -39,10 +28,5 @@ public class Source : MonoBehaviour {
 			createdBall.GetComponent<Ball>().init(new Vector2(0, -1));
 			yield return new WaitForSeconds(1);
 		}
-	}
-
-	// Update is called once per frame
-	void Update () {
-
 	}
 }
