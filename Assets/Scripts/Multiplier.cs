72547cd1e13ee069262f446c451628c29c87af72
Anthony Kim <anthonykim02@berkeley.edu>
2017-11-01 19:51:50 -0700

    added wall gizmo and some changes to magnet

---
+++ Assets/Scripts/Multiplier.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiplier : MonoBehaviour {
+	public GameObject ball;
+	public GameObject multiplier;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// thing to consider, things can fall at an angle so get the velocity of whatever is contacted
+	void OnTriggerEnter2D(Collider2D collider) {
+		switch (collider.tag) {
+		case "Ball": {
+				var v = collider.GetComponent<Rigidbody2D> ().velocity;
+
+
+				for (int i = 0; i < 5; i++) {
+					var w = multiplier.GetComponent<BoxCollider2D> ().bounds.size.x;
+					var rand = Random.Range (0, w);
+					GameObject createdBall = Instantiate (ball);
+					createdBall.GetComponent<Rigidbody2D> ().velocity = v * 0.5f;
+					createdBall.transform.position = new Vector2(transform.position.x - (w / 2) + rand, transform.position.y - 0.5f);
+				}
+				Destroy (multiplier);
+
+				break;
+			}
+		default: {
+				break;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
