1e2f2186d56aafc9d540f4cafa3667e29253de31
zane-c <zanedougall@gmail.com>
2017-11-14 02:15:49 -0800

    Added paralaxing to mainMenu.

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -11,9 +11,16 @@ public class CameraMove : MonoBehaviour {
 
 	private float speed = 3;
 
+	public float SCALING_CONSTANT;
+	private MeshRenderer mr;
+	private Material material;
+	private Vector2 offset;
+
 	// Use this for initialization
 	void Start () {
-		
+		mr = transform.GetChild(0).gameObject.GetComponent<MeshRenderer>();
+		material = mr.material;
+		offset = material.mainTextureOffset;
 	}
 	
 	// Update is called once per frame
@@ -21,18 +28,26 @@ public class CameraMove : MonoBehaviour {
 		if (panDown > 0) {
 			transform.position = new Vector3(transform.position.x,transform.position.y - speed * Time.deltaTime, transform.position.z);
 			panDown -= Time.deltaTime;
+			offset.y -= Time.deltaTime / 5;
+			material.mainTextureOffset = offset;
 		}
 		if (panUp > 0) {
 			transform.position = new Vector3(transform.position.x,transform.position.y + speed * Time.deltaTime, transform.position.z);
 			panUp -= Time.deltaTime;
+			offset.y += Time.deltaTime / 5;
+			material.mainTextureOffset = offset;
 		}
 		if (panLeft > 0) {
 			transform.position = new Vector3(transform.position.x,transform.position.y - speed * Time.deltaTime, transform.position.z);
 			panLeft -= Time.deltaTime;
+			offset.x -= Time.deltaTime / 5;
+			material.mainTextureOffset = offset;
 		}
 		if (panRight > 0) {
 			transform.position = new Vector3(transform.position.x,transform.position.y + speed * Time.deltaTime, transform.position.z);
 			panRight -= Time.deltaTime;
+			offset.y += Time.deltaTime / 5;
+			material.mainTextureOffset = offset;
 		}
 	}
 
---
+++ Assets/Scripts/FadeScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScreen : MonoBehaviour {
+
+	private SpriteRenderer sprite;
+	public double decrement = 0.0000000000001;
+
+	// Use this for initialization
+	void Start () {
+		sprite = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color tempColor = sprite.color;
+		if (tempColor.a > 0.001f) {
+			tempColor.a -= (float)decrement;
+			sprite.color = tempColor;
+		} else {
+			Destroy (this);
+		}
+	}
+}
