33fa7ad98503a8a95eb761c687988c892c1e4e6d
zane-c <zanedougall@gmail.com>
2017-11-14 03:42:13 -0800

    Fixed screen issue.

--- Assets/Scripts/LogoMover.cs
+++ Assets/Scripts/LogoMover.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class LogoMover : MonoBehaviour {
 
 	private float fixedY;
+	private float fixedX;
 	public GameObject ghost;
+	private float distance = 0.8f;
+	private float scale = 0.3f;
+	private bool goingUp = true;
 
 	// Use this for initialization
 	void Start () {
 		fixedY = transform.position.y;
+		fixedX = transform.position.x;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		double radians = (Time.realtimeSinceStartup * 2) % (2 * System.Math.PI);
 		float ySpeed = (float) (0.25 * System.Math.Cos (radians));
-		transform.position = new Vector3 (transform.position.x, fixedY + ySpeed, 0);
-
+		transform.position = new Vector3 (fixedX, fixedY + ySpeed, 0);
 		Instantiate (ghost, transform.position, transform.rotation);
+
+		//if (goingUp) {
+		//	transform.position = new Vector3 (transform.position.x, transform.position.y + scale * Time.deltaTime, 0);
+		//	distance -= Time.deltaTime;
+		//} else {
+		//	transform.position = new Vector3 (transform.position.x, transform.position.y - scale * Time.deltaTime, 0);
+		//	distance -= Time.deltaTime;
+		// }
+		// if (distance < 0) {
+		//	distance = 0.8f;
+		// 	goingUp = !goingUp;
+		// }
 	}
 }
