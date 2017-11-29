e8b42f6ff18fc5699b142b68b2f86f1a8b1a7bbb
zane-c <zanedougall@gmail.com>
2017-11-28 17:04:59 -0800

    flux lines freeze when game is paused.

--- Assets/Scripts/FluxLines.cs
+++ Assets/Scripts/FluxLines.cs
@@ -6,10 +6,9 @@ public class FluxLines : MonoBehaviour {
 
 	private float lifespan = 2.0f;
 
-	// Update is called once per frame
 	void Update () {
 		lifespan -= Time.deltaTime;
-		if (transform.localScale.x > 0) {
+		if (transform.localScale.x > 0 && Time.timeScale > 0) {
 			transform.localScale -= new Vector3 (0.009f, 0.0009f, 0);
 		}
 		if (lifespan < 0)
